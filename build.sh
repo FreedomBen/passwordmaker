cd511f01cf1bd73af39b6f2f5f5315f99b8e82e0
Benjamin Porter <FreedomBen@users.noreply.github.com>
2020-03-17 17:05:35 -0600

    Add docker mirrors

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 sudo podman build \
   -t quay.io/freedomben/passwordmaker:latest \
   -t quay.io/freedomben/passwordmaker:2.5 \
+  -t docker.io/freedomben/passwordmaker:latest \
+  -t docker.io/freedomben/passwordmaker:2.5 \
   .
--- push.sh
+++ push.sh
@@ -2,5 +2,14 @@
 
 set -e
 
-sudo podman push quay.io/freedomben/passwordmaker:latest
-sudo podman push quay.io/freedomben/passwordmaker:2.5
+sudo podman push --authfile ~/.docker/config.json \
+  quay.io/freedomben/passwordmaker:latest
+
+sudo podman push --authfile ~/.docker/config.json \
+  quay.io/freedomben/passwordmaker:2.5
+
+sudo podman push --authfile ~/.docker/config.json \
+  docker.io/freedomben/passwordmaker:latest
+
+sudo podman push --authfile ~/.docker/config.json \
+  docker.io/freedomben/passwordmaker:2.5
